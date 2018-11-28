61e25f1677d4eef67e6d37844bb559220d2debfb
alexandra_onofrei <alexandra.m.onofrei@gmail.com>
2018-11-28 14:05:41 +0200

    Soldier class created

---
+++ src/main/java/be/cegeka/battle/Weapon.java
@@ -0,0 +1,22 @@
+package be.cegeka.battle;
+
+public class Weapon {
+    public String name;
+    public int damage;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+}
